9155f5fd0dc2fe7a136a0d70b536936027afb3ab
Hatsumemi/Judith <judithgarnier4@gmail.com>
2024-11-18 00:19:29 +0100

    Menu principal

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class MainMenu : MonoBehaviour
+{
+    public Image Fade;
+
+    public GameObject Settings;
+    public GameObject Credits;
+
+    private void Awake()
+    {
+        StartCoroutine(FadingStart());
+    }
+
+    public void ButtonPlay()
+    {
+        StartCoroutine(FadingPlay());
+    }
+
+    public void ButtonSettings()
+    {
+        Settings.SetActive(true);
+    }
+
+    public void ButtonCredits()
+    {
+        Credits.SetActive(true);
+    }
+
+    public void ButtonBack()
+    {
+        Settings.SetActive(false);
+        Credits.SetActive(false);
+    }
+
+    public void ButtonQuit()
+    {
+        //If we are running in a standalone build of the game
+#if UNITY_STANDALONE
+        //Quit the application
+        StartCoroutine(FadingQuit());
+#endif
+
+        //If we are running in the editor
+#if UNITY_EDITOR
+        //Stop playing the scene
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    IEnumerator FadingStart()
+    {
+        Fade.DOFade(0, 3);
+        yield return new WaitForSeconds(3f);
+        Fade.enabled = false;
+    }
+
+
+    IEnumerator FadingPlay()
+    {
+        Fade.enabled = true;
+        Fade.DOFade(1, 2);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(1);
+    }
+
+    IEnumerator FadingQuit()
+    {
+        Fade.enabled = true;
+        Fade.DOFade(1, 2);
+        yield return new WaitForSeconds(2);
+    }
+
+}
