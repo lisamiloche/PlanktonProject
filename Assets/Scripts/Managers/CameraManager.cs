9822759ab886cb71586f7d73c01d602af38a0096
lisamiloche <124432385+lisamiloche@users.noreply.github.com>
2024-11-26 10:59:34 +0100

    differents trucs

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -11,6 +11,7 @@ public class CameraManager : MonoBehaviour
 {
     [SerializeField] private Transform _player;
     [SerializeField] private Transform _inventory;
+    [SerializeField] private Transform _boxDialog;
     [SerializeField] private Vector2 _minBounds;
     [SerializeField] private Vector2 _maxBounds;
     [SerializeField] private DialogManager[] _dialogManagers;
@@ -42,6 +43,11 @@ public class CameraManager : MonoBehaviour
 
             float clampedX = Mathf.Clamp(targetPos.x, _minBounds.x + _cameraWidth, _maxBounds.x - _cameraWidth);
             transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+
+            if(_boxDialog != null)
+            {
+                _boxDialog.transform.position = new Vector3(transform.position.x, _boxDialog.transform.position.y, _boxDialog.transform.position.z);
+            }
 
             _inventory.transform.position = new Vector3(transform.position.x, _inventory.transform.position.y, _inventory.transform.position.z);
         }
